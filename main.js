45bde94933de6783210089852b69870be7c2d29a
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-05-28 11:12:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-prediction_1 = "";
-prediction_2 = "";
+prediction = "";
+
 
 Webcam.set({
     width:350,
@@ -19,7 +19,7 @@ function takeSnapshot(){
 
 console.log("ml5 version : ",ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/2GUBZxfAM/model.json",modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/CC-P9D0CB/model.json",modelLoaded);
 
 function modelLoaded(){
     console.log("Model Loaded!");
@@ -27,8 +27,58 @@ function modelLoaded(){
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is "+prediction_1;
-    speak_data_2 = "And the second prediction is"+prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    speak_data_1 = prediction;
+    
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
+    console.log(utterThis);
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+
+        //prediction = results[0].label;
+
+        //speak();
+        
+        if(results[0].label=="Best"){
+            prediction="All the best";
+            document.getElementById("update_emoji").innerHTML="&#128077;";
+            speak();
+        }
+        if(results[0].label=="Call Me"){
+            prediction="You can call me now";
+            document.getElementById("update_emoji").innerHTML="&#129305;";
+            speak();
+        }
+        if(results[0].label=="Victory"){
+            prediction="A marvellous victory";
+            document.getElementById("update_emoji").innerHTML="&#x270C;";
+            speak();
+        }
+        if(results[0].label=="Amazing"){
+            prediction="This is amazing";
+            document.getElementById("update_emoji").innerHTML="&#x1F44C;";
+            speak();
+        }
+        if(results[0].label=="Namaste"){
+            prediction="Welcome";
+            document.getElementById("update_emoji").innerHTML="&#x1F64F;";
+            speak();
+        }
+
+       
+      
+    }
 }
